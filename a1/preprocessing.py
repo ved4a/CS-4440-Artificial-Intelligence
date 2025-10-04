40a1ec6d7d60e4a81d7fa1c62b2fe65d3e9e3155
vedaa <vedaas.anand@gmail.com>
2025-10-04 14:00:11 +0530

    feat: cnonicalize names

--- a1/preprocessing.py
+++ a1/preprocessing.py
@@ -1,8 +1,18 @@
 import cv2
 import os
 import numpy as np
+import re
 
-def loadImages(dataDirectory, size=(100, 100)):
+import re
+
+def canonicalize_name(name: str) -> str:
+    name = name.lower()
+    name = re.sub(r'[^a-z]', '', name)
+    return name
+
+
+
+def loadImages(dataDirectory, size=(100, 100), allowedIDs=None):
     images, labels = [],[]
     labelMap = {}
     counter  = 0
@@ -12,8 +22,11 @@ def loadImages(dataDirectory, size=(100, 100)):
         if not os.path.isdir(personPath):
             continue
 
-        if person not in labelMap:
-            labelMap[person] = counter
+        canonical = canonicalize_name(person)
+        if allowedIDs and canonical not in allowedIDs:
+            continue
+        if canonical not in lavelMap:
+            labelMap[canonical] = counter
             counter += 1
         
         for imageName in os.listdir(personPath):
@@ -27,6 +40,6 @@ def loadImages(dataDirectory, size=(100, 100)):
 
             imageResized = cv2.resize(image, size)
             images.append(imageResized.flatten())
-            labels.append(labelMap[person])
+            labels.append(labelMap[canonical])
     
     return np.array(images), np.array(labels), labelMap
